7dbd659c77b103d7fd28664c3941939c23d7c649
Zac Sweers <zac.sweers@gmail.com>
2019-05-25 18:27:22 -0500

    Misc cleanups

--- copydynamic/src/test/kotlin/io/sweers/copydynamic/metadata/MetadataTest.kt
+++ copydynamic/src/test/kotlin/io/sweers/copydynamic/metadata/MetadataTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.sweers.copydynamic.metadata;
+package io.sweers.copydynamic.metadata
 
 import com.google.common.truth.Truth.assertThat
 import com.squareup.kotlinpoet.ClassName
