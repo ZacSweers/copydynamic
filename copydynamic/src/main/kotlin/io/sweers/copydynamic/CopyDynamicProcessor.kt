8308c5e5caa735f54797a57464c4cd51bb623aef
Zac Sweers <zac.sweers@gmail.com>
2019-05-25 20:59:31 -0500

    Revert "Remove filer/incremental support because kapt doesn't actually support this"
    This reverts commit b8a0b8d908c1193cebdcabf7280104777df6d621.

--- copydynamic/src/main/kotlin/io/sweers/copydynamic/CopyDynamicProcessor.kt
+++ copydynamic/src/main/kotlin/io/sweers/copydynamic/CopyDynamicProcessor.kt
@@ -41,7 +41,8 @@ import io.sweers.copydynamic.metadata.readClassData
 import io.sweers.copydynamic.metadata.readKotlinClassMetadata
 import io.sweers.copydynamic.metadata.readMetadata
 import kotlinx.metadata.jvm.KotlinClassMetadata
-import java.io.File
+import net.ltgt.gradle.incap.IncrementalAnnotationProcessor
+import net.ltgt.gradle.incap.IncrementalAnnotationProcessorType
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Filer
 import javax.annotation.processing.Messager
@@ -55,6 +56,7 @@ import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
 import javax.tools.Diagnostic.Kind.ERROR
 
+@IncrementalAnnotationProcessor(IncrementalAnnotationProcessorType.ISOLATING)
 @SupportedOptions(OPTION_GENERATED)
 @AutoService(Processor::class)
 class CopyDynamicProcessor : AbstractProcessor() {
@@ -82,7 +84,6 @@ class CopyDynamicProcessor : AbstractProcessor() {
   private lateinit var elements: Elements
   private lateinit var types: Types
   private lateinit var options: Map<String, String>
-  private lateinit var outputDir: File
   private var generatedAnnotation: AnnotationSpec? = null
 
   override fun init(processingEnv: ProcessingEnvironment) {
@@ -92,8 +93,6 @@ class CopyDynamicProcessor : AbstractProcessor() {
     elements = processingEnv.elementUtils
     types = processingEnv.typeUtils
     options = processingEnv.options
-    outputDir = options["kapt.kotlin.generated"]?.let(::File) ?: throw IllegalStateException(
-        "No kapt.kotlin.generated option provided")
     generatedAnnotation = options[OPTION_GENERATED]?.let {
       require(it in POSSIBLE_GENERATED_NAMES) {
         "Invalid option value for $OPTION_GENERATED. Found $it, allowable values are $POSSIBLE_GENERATED_NAMES."
@@ -176,6 +175,7 @@ class CopyDynamicProcessor : AbstractProcessor() {
     val sourceParam = ParameterSpec.builder(nameAllocator.newName("source"), sourceType).build()
     val properties = mutableListOf<Pair<String, PropertySpec>>()
     val builderSpec = TypeSpec.classBuilder(builderName)
+        .addOriginatingElement(element)
         .apply {
           generatedAnnotation?.let(::addAnnotation)
           if (visibility != PUBLIC) {
@@ -245,6 +245,6 @@ class CopyDynamicProcessor : AbstractProcessor() {
         .addType(builderSpec)
         .addFunction(extensionFun)
         .build()
-        .writeTo(outputDir)
+        .writeTo(filer)
   }
 }
