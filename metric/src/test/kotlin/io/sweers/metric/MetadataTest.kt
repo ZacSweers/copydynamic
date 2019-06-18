8c6af80ac08e943ed6c806de01665020e6198d6e
Zac Sweers <zac.sweers@gmail.com>
2019-06-18 00:15:04 -0500

    Suppress some more for the tests

--- metric/src/test/kotlin/io/sweers/metric/MetadataTest.kt
+++ metric/src/test/kotlin/io/sweers/metric/MetadataTest.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("NOTHING_TO_INLINE", "RedundantSuspendModifier", "RemoveRedundantQualifierName")
+
 package io.sweers.metric
 
 import com.google.common.truth.Truth.assertThat
